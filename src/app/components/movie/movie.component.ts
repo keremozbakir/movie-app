d9c91b0bb903213cf3609564a19e33b8403d1f89
keremozbakir <63881378+keremozbakir@users.noreply.github.com>
2022-08-17 01:58:37 +0200

    error fixed

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -9,18 +9,26 @@ import { TransporterService } from 'src/app/services/transporter.service';
 })
 export class MovieComponent implements OnInit {
 
-  constructor(private httpClient:HttpClient,private transporterService:TransporterService) { }
+  constructor(private httpClient: HttpClient, private transporterService: TransporterService) { }
 
   ngOnInit(): void {
     
-   //this.getMovies();
-    this.transporterService.searchWord.subscribe((msg:any) => {
+    this.getMovies();
+    this.transporterService.searchWord.subscribe((msg: any) => {
       this.searchParam = msg
     })
 
     
   }
   allMovies: any
-  searchParam: string ='stardust'
-  //getmovies heeree
+  searchParam: any = 'stardust'
+  params={q:this.searchParam}
+  getMovies() {
+    let url = 'https://online-movie-database.p.rapidapi.com/auto-complete'
+    let header = new HttpHeaders().set('X-RapidAPI-Key','b27b01202emsh3b40e8d45d9c12bp1923d8jsn9b7d447ff8a3').set('X-RapidAPI-Host', 'online-movie-database.p.rapidapi.com')
+    this.httpClient.get(url, { params: this.params, headers: header }).subscribe((movies) => {
+      console.log(movies)
+      this.allMovies =movies
+    })
+  }
 }
