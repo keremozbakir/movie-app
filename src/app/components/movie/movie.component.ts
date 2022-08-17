862afdcae4f2a0cb907975b6300b506ca12d0fd2
keremozbakir <63881378+keremozbakir@users.noreply.github.com>
2022-08-17 23:44:39 +0200

    search button activated

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { TransporterService } from 'src/app/services/transporter.service';
+import moviesJson from '../movie/movies.json'
+
 
 @Component({
   selector: 'app-movie',
@@ -13,20 +15,21 @@ export class MovieComponent implements OnInit {
 
   ngOnInit(): void {
     
-    this.getMovies();
+    //this.getMovies();
     this.transporterService.searchWord.subscribe((msg: any) => {
       this.searchParam = msg
+      //this.getMovies()
     })
 
-    
   }
-  allMovies: any
+  allMovies: any = moviesJson
   searchParam: any = 'stardust'
-  params={q:this.searchParam}
+  
   getMovies() {
+    var params={q:this.searchParam}
     let url = 'https://online-movie-database.p.rapidapi.com/auto-complete'
     let header = new HttpHeaders().set('X-RapidAPI-Key','b27b01202emsh3b40e8d45d9c12bp1923d8jsn9b7d447ff8a3').set('X-RapidAPI-Host', 'online-movie-database.p.rapidapi.com')
-    this.httpClient.get(url, { params: this.params, headers: header }).subscribe((movies) => {
+    this.httpClient.get(url, { params: params, headers: header }).subscribe((movies) => {
       console.log(movies)
       this.allMovies =movies
     })
